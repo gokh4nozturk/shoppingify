afc4e137f04764bee5f8d3005eb64923cc3497a2
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-02 23:32:00 +0300

    improvements

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navigation from "../Navigation";
-import Products from "../Products/Products";
-import ShoppingList from "../ShoppingList";
+import Navigation from "../../components/Navigation";
+import Products from "../../components/Products/Products";
+import ShoppingList from "../../components/ShoppingList";
 import Statistics from "../Statistics";
 import History from "../History";
 
 import { HomeContainer } from "./styled";
-
-import { ProductType } from "../Products/Product";
 
 const Home = () => {
   return (
