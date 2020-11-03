c544498685bb9e187fc0d2495bee93670508c7d2
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-03 23:27:06 +0300

    improvement

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { Logo } from "../icons/";
 
 import { NavContainer } from "./styled";
 import { MdList, MdReplay, MdShoppingCart } from "react-icons/md";
 import { BiStats } from "react-icons/bi";
+import { Shopping } from "../../context";
 
 const Navigation = () => {
+  const { cart } = useContext(Shopping);
   return (
     <NavContainer>
       <div className="nav-item logo">
@@ -27,7 +29,7 @@ const Navigation = () => {
         <div>
           <MdShoppingCart size="1.5em" />
         </div>
-        <span className="cart-badge">3</span>
+        <span className="cart-badge">{cart.length}</span>
       </div>
     </NavContainer>
   );
