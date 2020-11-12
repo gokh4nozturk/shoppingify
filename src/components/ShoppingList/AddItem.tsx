9bbabe6830857f4c4f5be69332e844cc1ee07a68
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-12 10:13:36 +0300

    updated post method

--- src/components/ShoppingList/AddItem.tsx
+++ src/components/ShoppingList/AddItem.tsx
@@ -1,4 +1,6 @@
-import React, { useContext } from "react";
+import Axios from "axios";
+import React, { useContext, useState } from "react";
+import { useCallback } from "react";
 import { BsArrowLeft } from "react-icons/bs";
 import { Shopping } from "../../context";
 import {
@@ -15,7 +17,29 @@ import {
 import { BackButton, BtnBack } from "./style/styleOverview";
 
 const AddItem = () => {
-  const { categories, isControlToggleAddItem } = useContext(Shopping);
+  const { categories, isControlToggleAddItem, fetchData } = useContext(
+    Shopping
+  );
+  const [name, setName] = useState("");
+  const [note, setNote] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [category, setCategory] = useState("");
+
+  const addToDB = useCallback(() => {
+    Axios.post("/api/product", {
+      name,
+      note,
+      category,
+      image: imageUrl,
+    })
+      .then(function (response) {
+        console.log(response);
+        fetchData();
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [name, category, note, imageUrl, fetchData]);
 
   return (
     <AddItemContainer>
@@ -33,16 +57,15 @@ const AddItem = () => {
       <TitleAddItem className="add-item-title">
         <p>Add a new item</p>
       </TitleAddItem>
-      <FormContainer
-        action="/api/product"
-        method="post"
-        target="_blank"
-        className="form"
-      >
+      <FormContainer className="form">
         <InputContainer>
           <label htmlFor="name-item">Name</label>
 
           <InputElement
+            value={name}
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+            }}
             type="text"
             name="name"
             id="name-item"
@@ -54,6 +77,10 @@ const AddItem = () => {
           <label htmlFor="note-item">Note (optional)</label>
 
           <InputElementArea
+            value={note}
+            onChange={(e) => {
+              setNote(e.currentTarget.value);
+            }}
             rows={6}
             name="note"
             id="note-item"
@@ -64,6 +91,10 @@ const AddItem = () => {
           <label htmlFor="image-item">Image (optional)</label>
 
           <InputElement
+            value={imageUrl}
+            onChange={(e) => {
+              setImageUrl(e.currentTarget.value);
+            }}
             type="text"
             name="image"
             id="image-item"
@@ -74,6 +105,10 @@ const AddItem = () => {
           <label htmlFor="image-item">Category</label>
 
           <InputElement
+            value={category}
+            onChange={(e) => {
+              setCategory(e.currentTarget.value);
+            }}
             type="text"
             list="category-items"
             placeholder="Enter a category"
@@ -88,14 +123,16 @@ const AddItem = () => {
           </DatalistCategories>
         </InputContainer>
         <BtnContainer>
-          <button
-            className="btn-cancel buttons"
-            type="reset"
-            onClick={() => {}}
-          >
+          <button className="btn-cancel buttons" type="reset">
             Cancel
           </button>
-          <button className="btn-save buttons" type="submit">
+          <button
+            className="btn-save buttons"
+            type="submit"
+            onClick={() => {
+              addToDB();
+            }}
+          >
             Save
           </button>
         </BtnContainer>
