07bad597ea8ca04c2d5b9e40cd54e1850265bc56
Gökhan Öztürk <gkhan.oztrk288@gmail.com>
2020-11-14 23:56:09 +0300

    top categories improvements

--- src/pages/Statistics/charts/pieCategories/index.tsx
+++ src/pages/Statistics/charts/pieCategories/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 
 import { PieChart, Pie, Tooltip } from "recharts";
+import { Shopping } from "../../../../context";
 
 const data01 = [
   { name: "Group A", value: 40, fill: "#84d88b" },
@@ -9,7 +10,43 @@ const data01 = [
   { name: "Group D", value: 20, fill: "#8884d8" },
 ];
 
+type data = {
+  name: string;
+  value: number;
+  fill: string;
+};
+
 const TwoSimplePieChart = () => {
+  const { history } = useContext(Shopping);
+  const [data, setData] = useState<data[]>([]);
+
+  const getTopCategories = useCallback(() => {
+    const histories = [...history];
+    const categories = histories
+      .map((item) => item.listItem)
+      .flat()
+      .map((item) => item.category)
+      .sort();
+
+    let count = 0;
+    const newCategories = categories.map((item, index) => {
+      if (item === categories[index - 1]) {
+        count++;
+      } else count = 0;
+      return { name: item, value: count + 1 };
+    });
+
+    const topCategories = newCategories.map((item, index) =>
+      item === newCategories[index - 1] ? null : "ok"
+    );
+
+    console.log(newCategories);
+  }, [history]);
+
+  useEffect(() => {
+    getTopCategories();
+  }, [getTopCategories]);
+
   return (
     <PieChart width={400} height={400}>
       <Pie
